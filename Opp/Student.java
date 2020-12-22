161283fb798289dc6a4197fd94bf1f93272383f5
MONZIM <monzim.code@gmail.com>
2020-12-22 19:22:16 +0600

    opp Student method add

--- Opp/Student.java
+++ Opp/Student.java
@@ -5,4 +5,12 @@ public class Student {
     String name, gender;
     int number, roll, age;
 
+    void displayInformation() {
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Number: " + number);
+        System.out.println("Roll: " + roll + "\n");
+    }
+
 }
--- Opp/StudentMain.java
+++ Opp/StudentMain.java
@@ -11,26 +11,17 @@ public class StudentMain {
         student1.age = 18;
         student1.number = 1236548;
         student1.roll = 1692;
-
-        System.out.println("Student1 name: " + student1.name);
-        System.out.println("Student1 gender: " + student1.gender);
-        System.out.println("Student1 age: " + student1.age);
-        System.out.println("Student1 number: " + student1.number);
-        System.out.println("Student1 roll: " + student1.roll);
+        student1.displayInformation();
 
         Student student2 = new Student();
 
         student2.name = " Monirul Islam Shamim";
         student2.gender = "male";
-        student2.age = 8;
+        student2.age = 18;
         student2.number = 56565;
         student2.roll = 1697;
 
-        System.out.println("\nStudent2 name: " + student2.name);
-        System.out.println("Student2 gender: " + student2.gender);
-        System.out.println("Student2 age: " + student2.age);
-        System.out.println("Student2 number: " + student2.number);
-        System.out.println("Student2 roll: " + student2.roll);
+        student2.displayInformation();
 
     }
 
